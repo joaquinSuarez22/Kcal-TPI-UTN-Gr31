65bca7271d281e8fd74eed289430bf500bfa120b
Tadeo <tadeolg231104@gmail.com>
2026-03-20 18:45:46 -0300

    Conexion base de datos

--- app/src/main/java/com/example/tif_gr31/activities/LoginActivity.java
+++ app/src/main/java/com/example/tif_gr31/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tif_gr31.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,6 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         /// botones
         Button BtnRegistrar=findViewById(R.id.BtnRegistrar);
         Button BtnMenu=findViewById(R.id.loginBtn);
+        Button btnTest= findViewById(R.id.loginBtn);
 
         ///  el evento click por si no esta registrado
         BtnRegistrar.setOnClickListener(new View.OnClickListener() {
@@ -39,16 +42,29 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+        btnTest.setOnClickListener(v -> {
 
+            FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword("a@a.com", "123456")
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            Toast.makeText(this, "OK FIREBASE", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        });
 
         /// evento para ir al menu
-        BtnMenu.setOnClickListener(new View.OnClickListener() {
+      BtnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(LoginActivity.this, InicioActivity.class);
                 startActivity(intent);
             }
         });
+
 
 
     }
