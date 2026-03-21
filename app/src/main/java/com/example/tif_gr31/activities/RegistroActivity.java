4c766eb29bc97bd34ac887c3c798ae6c253d4ec6
Tadeo <tadeolg231104@gmail.com>
2026-03-21 16:42:31 -0300

    Conexion Firestore

--- app/src/main/java/com/example/tif_gr31/activities/RegistroActivity.java
+++ app/src/main/java/com/example/tif_gr31/activities/RegistroActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tif_gr31.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firestore.v1.Value;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistroActivity extends AppCompatActivity {
 
+   private FirebaseFirestore db= FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +37,31 @@ public class RegistroActivity extends AppCompatActivity {
         /// botones
         Button BtnConfirmar=findViewById(R.id.BtnRegistrar);
         Button BtnRegresar=findViewById(R.id.btnYaCuenta);
+        EditText txtEmail=findViewById(R.id.emailRegister);
+        EditText txtPassword=findViewById(R.id.passwordRegister);
+        EditText txtRepPass=findViewById(R.id.repeatPassword);
+
 
         /// evento para ir al menu
-        BtnConfirmar.setOnClickListener(new View.OnClickListener() {
+        BtnConfirmar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(RegistroActivity.this, InicioActivity.class);
-                startActivity(intent);
+                String email = txtEmail.getText().toString();
+                String pass = txtPassword.getText().toString();
+                String repetir = txtRepPass.getText().toString();
+
+                Map<String, Object> usuario = new HashMap<>();
+                usuario.put("email", email);
+                usuario.put("password", pass);
+
+                // Guardar en Firestore
+                db.collection("usuarios")
+                        .add(usuario)
+                        .addOnSuccessListener(documentReference -> {
+                            Toast.makeText(RegistroActivity.this, "Usuario guardado ", Toast.LENGTH_SHORT).show();
+                        });
+
+
             }
         });
         /// evento clcik para volver al login
